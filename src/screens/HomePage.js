76350504eb45ea582091f64dde3c5500cbab80c4
Edouard Misset <edouardmisset@gmail.com>
2021-06-28 17:36:26 +0200

    Started UserPage

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,9 +1,11 @@
 import Article from '../components/Article';
+import UserPage from './UserPage';
 
 export default function HomePage() {
   return (
     <div>
       <Article />
+      <UserPage />
     </div>
   );
 }
