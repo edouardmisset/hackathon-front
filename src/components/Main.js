7302dabe9fa10e57f96d1aec71026f82a15fcf6d
Edouard Misset <edouardmisset@gmail.com>
2021-06-28 19:18:18 +0200

    Search bar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import EventsPage from '../screens/EventsPage';
 import HomePage from '../screens/HomePage';
+import SearchPage from '../screens/SearchPage';
 
 export default function Main() {
   return (
@@ -8,6 +9,7 @@ export default function Main() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/events" component={EventsPage} />
+        <Route exact path="/search" component={SearchPage} />
       </Switch>
     </main>
   );
