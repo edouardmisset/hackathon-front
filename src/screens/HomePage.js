9316f7dfa076cdffab99db28785fa209a7bb823d
Edouard Misset <edouardmisset@gmail.com>
2021-06-28 21:59:43 +0200

    removed the userpage from homepage

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,12 +1,10 @@
 import Article from '../components/Article';
-import UserPage from './UserPage';
 
 export default function HomePage() {
   return (
     <div>
       <h2 className="text-center text-lg">Home</h2>
       <Article />
-      <UserPage />
     </div>
   );
 }
