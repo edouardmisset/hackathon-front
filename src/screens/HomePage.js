fa8c34ef4293257d2ce1b3012f5199619b7c7518
Edouard Misset <edouardmisset@gmail.com>
2021-06-29 16:30:56 +0200

    Removed Event Page from nav

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import API from '../APIClient';
 import Events from '../components/Events';
+import SearchBar from '../components/SearchBar';
 
 export default function HomePage() {
   const [recentEvents, setRecentEvents] = useState([]);
@@ -16,12 +17,13 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div>
+    <>
+      <SearchBar />
       <h2 className="text-center text-lg">Recent Events</h2>
       <Events eventList={recentEvents} />
       <h2 className="text-center text-lg">Popular Events</h2>
       <Events eventList={popularEvents} />
       <h2 className="text-center text-lg">My Events</h2>
-    </div>
+    </>
   );
 }
