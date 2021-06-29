d998bccd472ec945ae71c9a6836074f35f80d038
Edouard Misset <edouardmisset@gmail.com>
2021-06-29 16:19:33 +0200

    refacto Events.js to be a dumb component & passed data from the home page

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,28 +1,11 @@
-import { useState, useEffect } from 'react';
-import API from '../APIClient';
 import Event from './Event';
 
-export default function Events() {
-  const [eventsList, setEventsList] = useState([]);
-
-  useEffect(() => {
-    API.get('/events')
-      .then((res) => {
-        setEventsList(res.data);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
+export default function Events({ eventList }) {
   return (
     <div className="flex flex-col justify-center p-5 ">
-      <div className="titre">
-        <h1 className="mt-6 text-3xl font-extrabold">Evènements à venir</h1>
-      </div>
-      <br />
-
       <ul className="flex w-full overflow-x-scroll">
-        {eventsList &&
-          eventsList.map(
+        {eventList.length &&
+          eventList.map(
             ({ id, name, image, categories, location, date, duration }) => (
               <li className="min-w-max" key={id}>
                 <Event
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+import API from '../APIClient';
+import Events from '../components/Events';
+
 export default function HomePage() {
+  const [recentEvents, setRecentEvents] = useState([]);
+  const [popularEvents, setPopularEvents] = useState([]);
+
+  useEffect(() => {
+    API.get('/events/popular').then((res) => {
+      setPopularEvents(res.data);
+    });
+    API.get('/events/upcoming').then((res) => {
+      setRecentEvents(res.data);
+    });
+  }, []);
+
   return (
     <div>
       <h2 className="text-center text-lg">Home</h2>
       <h3>Recent Events</h3>
+      <Events eventList={recentEvents} />
       <h3>Popular Events</h3>
+      <Events eventList={popularEvents} />
       <h3>My Events</h3>
     </div>
   );
