d12c3c5b6b93ca4e87a0ce9831d53bbabe982ab9
Edouard Misset <edouardmisset@gmail.com>
2021-06-29 16:25:08 +0200

    Styled home page

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -17,12 +17,11 @@ export default function HomePage() {
 
   return (
     <div>
-      <h2 className="text-center text-lg">Home</h2>
-      <h3>Recent Events</h3>
+      <h2 className="text-center text-lg">Recent Events</h2>
       <Events eventList={recentEvents} />
-      <h3>Popular Events</h3>
+      <h2 className="text-center text-lg">Popular Events</h2>
       <Events eventList={popularEvents} />
-      <h3>My Events</h3>
+      <h2 className="text-center text-lg">My Events</h2>
     </div>
   );
 }
